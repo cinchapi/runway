93a96e036fe475317a5a70686f0a50642742d046
Jeff Nelson <jeff@cinchapi.com>
2016-12-24 10:05:54 -0600

    refactor

--- src/main/java/com/cinchapi/runway/importer/MagicRunwayFileLineImporter.java
+++ src/main/java/com/cinchapi/runway/importer/MagicRunwayFileLineImporter.java
@@ -1,19 +1,8 @@
 package com.cinchapi.runway.importer;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-
 import com.cinchapi.concourse.Concourse;
 import com.cinchapi.runway.Record;
-import com.google.common.base.Throwables;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
-import com.google.common.collect.Sets;
 
 // TODO test me please
 
@@ -35,61 +24,13 @@ public abstract class MagicRunwayFileLineImporter<T extends Record> extends
         super(concourse);
     }
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
     @Override
     protected void importData(T record, Multimap<String, Object> data) {
-        try {
-            Method method = Record.class
-                    .getDeclaredMethod("getAllDeclaredFields");
-            method.setAccessible(true);
-            Field[] fields = (Field[]) method.invoke(record);
-            for (Field field : fields) {
-                try {
-                    Collection collection = null;
-                    if(Collection.class.isAssignableFrom(field.getType())) {
-                        if(Modifier.isAbstract(field.getType().getModifiers())
-                                || Modifier.isInterface(field.getType()
-                                        .getModifiers())) {
-                            if(field.getType() == Set.class){
-                                collection = Sets.newLinkedHashSet();
-                            }
-                            else { //assume list
-                                collection = Lists.newArrayList();
-                            }
-                        }
-                        else {
-                            collection = (Collection) field.getType()
-                                    .newInstance();
-                        }
-                        for (Object obj : Imports.getAll(field.getName(), data)) {
-                            collection.add(obj);
-                        }
-                        field.set(record, collection);
-                    }
-                    else if(field.getType().isArray()) {
-                        List list = new ArrayList();
-                        for (Object obj : Imports.getAll(field.getName(), data)) {
-                            list.add(obj);
-                        }
-                        field.set(record, list.toArray());
-                    }
-                    else if(field.getType().isEnum()) {
-                        field.set(record, Enum.valueOf(
-                                (Class<Enum>) field.getType(),
-                                Imports.<String> get(field.getName(), data)));
-                    }
-                    else {
-                        field.set(record, Imports.get(field.getName(), data));
-                    }
-                }
-                catch (Exception e) {
-                    continue;
-                }
-            }
-        }
-        catch (ReflectiveOperationException e) {
-            Throwables.propagate(e);
-        }
+        data.asMap().forEach((key, values) ->{
+            values.forEach((value) -> {
+                record.set(key, value);
+            });
+        });
     }
 
 }
