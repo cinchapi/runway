2b423a8917434f86eafa972f84c76cae7ed4bdb9
Jeff Nelson <jeff@cinchapi.com>
2019-08-06 16:28:48 -0400

    update version for test

--- src/test/java/com/cinchapi/runway/RunwayCrossVersionTest.java
+++ src/test/java/com/cinchapi/runway/RunwayCrossVersionTest.java
@@ -39,7 +39,7 @@ import com.cinchapi.concourse.util.Random;
  *
  * @author Jeff Nelson
  */
-@Versions({ "0.9.6", "0.10.0" })
+@Versions({ "0.9.6", "0.10.1" })
 public class RunwayCrossVersionTest extends CrossVersionTest {
 
     private Runway runway;
