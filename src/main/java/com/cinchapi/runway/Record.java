e19e1741a47f15de856f87c407201a92ea8b7365
Jeff Nelson <jeff@cinchapi.com>
2022-04-28 12:01:58 -0400

    clarity

--- src/main/java/com/cinchapi/runway/Record.java
+++ src/main/java/com/cinchapi/runway/Record.java
@@ -1387,7 +1387,7 @@ public abstract class Record implements Comparable<Record> {
     /* package */ @SuppressWarnings({ "rawtypes", "unchecked" })
     final void load(Concourse concourse, TLongObjectMap<Record> existing,
             @Nullable Map<String, Set<Object>> data, @Nullable String prefix) {
-        prefix = prefix == null || !runway.supportsPreSelectLinkedRecord ? ""
+        prefix = (prefix == null || !runway.supportsPreSelectLinkedRecord) ? ""
                 : prefix;
         Preconditions.checkState(id != NULL_ID);
         existing.put(id, this); // add the current object so we don't
